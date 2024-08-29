694b959a4f233a804b8c7167aea1652788191f45
mahsan2002 <zcemahs@ucl.ac.uk>
2024-08-29 11:55:23 +0100

    Add file

---
+++ GIT_Starter_Flask_App/application/routes.py
@@ -0,0 +1,20 @@
+from flask import render_template
+
+from application import app
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
+
+
+@app.route('/welcome/<name>')
+def welcome(name):
+    return render_template('welcome.html', name=name, group='Everyone')
+
+
+@app.route('/contact')
+def contact_us():
+    # code goes here
+    return render_template('contact.html', location='Osterley', title='Contact Us')
