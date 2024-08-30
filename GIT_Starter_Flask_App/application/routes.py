9fad893602a8759e1a647ae554e92815ba286637
IsaiahMoses <138685197+IsaiahMoses@users.noreply.github.com>
2024-08-30 15:27:15 +0100

    Victoria login and register

--- GIT_Starter_Flask_App/application/routes.py
+++ GIT_Starter_Flask_App/application/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from application import app
+from application.my_connector import get_users, add_user
+from application.login import is_strong_password
 
 
 @app.route('/')
@@ -38,3 +40,30 @@ def leaderboard():
 def tracker():
     # code goes here
     return render_template('tracker.html', title='Tracker')
+
+@app.route('/users')
+def all_users_from_db():
+    users_from_db = get_users()
+    print(users_from_db)
+    return render_template('users.html', users=users_from_db, title='Database People')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = ""
+
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        password_strong = is_strong_password(password)
+
+        if password_strong[0] == False:
+            error = password_strong[1]
+
+        else:
+
+            add_user(username, password)
+            return redirect(url_for('home'))
+
+    return render_template('register.html', title='Add User', message=error)
